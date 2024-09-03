0edf67681dd015b558b39f67335230610465116a
Carlos Silva <carlosqsilva@outlook.com>
2024-09-03 17:14:17 -0300

    update scraper deps

--- scraper/main.ts
+++ scraper/main.ts
@@ -1,47 +1,51 @@
 import {
-  Browser,
-  BrowserErrorCaptureEnum,
-  BrowserNavigationCrossOriginPolicyEnum,
+	Browser,
+	BrowserErrorCaptureEnum,
+	BrowserNavigationCrossOriginPolicyEnum,
 } from "happy-dom";
 import { Storage } from "./database.ts";
 import { extractThisWeekInReact } from "./newsletter/thisweekinreact.ts";
 import { extractJavascriptlyWeekly } from "./newsletter/javascripweekly/index.ts";
 import {
-  extractFrontendFocus,
-  extractGolangWeekly,
-  extractNodeWeekly,
-  extractPostgresWeekly,
-  extractReactStatus,
-  extractRubyWeekly,
+	extractFrontendFocus,
+	extractGolangWeekly,
+	extractNodeWeekly,
+	extractPostgresWeekly,
+	extractReactStatus,
+	extractRubyWeekly,
 } from "./newsletter/common.ts";
 
 const storage = new Storage();
 
 const browser = new Browser({
-  settings: {
-    errorCapture: BrowserErrorCaptureEnum.processLevel,
-    disableCSSFileLoading: true,
-    disableJavaScriptFileLoading: true,
-    disableJavaScriptEvaluation: true,
-    navigation: {
-      crossOriginPolicy: BrowserNavigationCrossOriginPolicyEnum.anyOrigin,
-    },
-  },
+	settings: {
+		errorCapture: BrowserErrorCaptureEnum.processLevel,
+		disableCSSFileLoading: true,
+		disableJavaScriptFileLoading: true,
+		disableJavaScriptEvaluation: true,
+		navigation: {
+			crossOriginPolicy: BrowserNavigationCrossOriginPolicyEnum.anyOrigin,
+		},
+		navigator: {
+			userAgent:
+				"Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/128.0.0.0 Safari/537.36",
+		},
+	},
 });
 
 try {
-  await Promise.all([
-    extractJavascriptlyWeekly(browser, storage),
-    extractFrontendFocus(browser, storage),
-    extractGolangWeekly(browser, storage),
-    extractNodeWeekly(browser, storage),
-    extractPostgresWeekly(browser, storage),
-    extractReactStatus(browser, storage),
-    extractRubyWeekly(browser, storage),
-    extractThisWeekInReact(browser, storage),
-  ]);
+	await Promise.all([
+		extractJavascriptlyWeekly(browser, storage),
+		extractFrontendFocus(browser, storage),
+		extractGolangWeekly(browser, storage),
+		extractNodeWeekly(browser, storage),
+		extractPostgresWeekly(browser, storage),
+		extractReactStatus(browser, storage),
+		extractRubyWeekly(browser, storage),
+		extractThisWeekInReact(browser, storage),
+	]);
 } catch (err) {
-  console.log(err);
+	console.log(err);
 }
 
 await browser.close();
